88ef16fa592787da93673718f8d5e5d1552b147a
Alexander Sklar <asklar@microsoft.com>
2021-12-22 02:49:16 -0500

    Add files via upload

---
+++ search/all_4.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['get_0',['Get',['../structcppxaml_1_1_xaml_window.html#af69bf7a9f244b8c5386d1cb6a4250fac',1,'cppxaml::XamlWindow']]],
+  ['getuielement_1',['GetUIElement',['../structcppxaml_1_1_xaml_window.html#a88e72ce8b26661cdd7d28c4deccc4e22',1,'cppxaml::XamlWindow']]],
+  ['grid_2',['Grid',['../namespacecppxaml.html#a55c9314e2f4f12c848a791ec95c3dda6',1,'cppxaml']]]
+];

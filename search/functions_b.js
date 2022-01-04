e5c25660a34460f0678f855d98c9532c20150f74
asklar <asklar@users.noreply.github.com>
2022-01-04 23:21:17 +0000

    deploy: f2fe54c6d0e28d315b9c10eaa04b3929ab540a37

--- search/functions_b.js
+++ search/functions_b.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['runloop_0',['RunLoop',['../structcppxaml_1_1_xaml_window.html#ab744fd56f45f6e30ebb02d178f9e6161',1,'cppxaml::XamlWindow']]]
+  ['raisepropertychanged_0',['RaisePropertyChanged',['../structcppxaml_1_1_simple_notify_property_changed.html#ade150fab4ab5498eafec1d11984d06c8',1,'cppxaml::SimpleNotifyPropertyChanged']]],
+  ['runloop_1',['RunLoop',['../structcppxaml_1_1_xaml_window.html#ab744fd56f45f6e30ebb02d178f9e6161',1,'cppxaml::XamlWindow']]]
 ];

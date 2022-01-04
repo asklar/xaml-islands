3fc9879af1b467ac51ddd9b5700b54b3edb82571
Alexander Sklar <asklar@microsoft.com>
2022-01-04 18:17:52 -0500

    doxygen->docfx early prototype

---
+++ docfx_project/src/HelloDocfx/Class1.cs
@@ -0,0 +1,41 @@
+﻿namespace HelloDocfx
+{
+    /// <summary>
+    /// Hello this is **Class1** from *HelloDocfx*
+    /// </summary>
+    public class Class1
+    {
+        private InnerClass _class;
+        public int Value { get; }
+
+        /// <summary>
+        /// This is a ctor
+        /// </summary>
+        /// <param name="value">The value of the class</param>
+        public Class1(int value)
+        {
+            Value = value;
+        }
+
+        public double ConvertToDouble()
+        {
+            return Value;
+        }
+
+        /// <summary>
+        /// A method referencing a inner class
+        /// </summary>
+        /// <param name="name">The name</param>
+        /// <param name="inner">A inner class with type <seealso cref="InnerClass"/></param>
+        public void SetInnerClass(string name, InnerClass inner)
+        {
+            inner.Name = name;
+            _class = inner;
+        }
+
+        public class InnerClass
+        {
+            public string Name { get; set; }
+        }
+    }
+}

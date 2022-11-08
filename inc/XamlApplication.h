a8f118ce6c6a62b08d03cb875f565d80b5e576c0
Alexander Sklar <asklar@microsoft.com>
2022-11-08 01:21:45 -0800

    Use idl

--- inc/XamlApplication.h
+++ inc/XamlApplication.h
@@ -1,87 +1,107 @@
 #pragma once
 
-#include "XamlApplication.g.h"
+#include "CppXaml.XamlApplication.g.h"
 #include <winrt/Windows.UI.Xaml.Hosting.h>
-#include <winrt/Windows.UI.ViewManagement.h>
-#include <winrt/Windows.Foundation.Collections.h>
-#include <Windows.h>
-//#include <DesktopWindow.h>
-namespace winrt::Microsoft::Toolkit::Win32::UI::XamlHost {
-  struct XamlApplication;
+
+namespace winrt::CppXaml::implementation
+{
+    struct XamlApplication : XamlApplicationT<XamlApplication>
+    {
+        XamlApplication() = default;
+
+        XamlApplication(winrt::Windows::Foundation::Collections::IVector<winrt::Windows::UI::Xaml::Markup::IXamlMetadataProvider> const& providers)
+        {
+            for (auto&& provider : providers)
+            {
+                m_providers.Append(provider);
+            }
+        }
+
+        ~XamlApplication()
+        {
+            Close();
+        }
+
+        void InitializeComponent()  // C++ WinRT 2 phase construction
+        {
+            m_windowsXamlManager = winrt::Windows::UI::Xaml::Hosting::WindowsXamlManager::InitializeForCurrentThread();
+        }
+
+        winrt::Windows::Foundation::Collections::IVector<winrt::Windows::UI::Xaml::Markup::IXamlMetadataProvider> MetadataProviders()
+        {
+            return m_providers;
+        }
+
+        winrt::Windows::Foundation::IClosable WindowsXamlManager()
+        {
+            return m_windowsXamlManager;
+        }
+
+        // ICloseable
+        void Close()
+        {
+            if (b_closed) return;
+
+            b_closed = true;
+
+            m_windowsXamlManager.Close();
+            m_providers.Clear();
+            m_windowsXamlManager = nullptr;
+
+            // Toolkit has a message loop here.
+        }
+
+        // IXamlMetadataProvider
+        winrt::Windows::UI::Xaml::Markup::IXamlType GetXamlType(winrt::Windows::UI::Xaml::Interop::TypeName const& type)
+        {
+            for (const auto& provider : m_providers)
+            {
+                if (const auto xamlType = provider.GetXamlType(type))
+                {
+                    return xamlType;
+                }
+            }
+            return nullptr;
+        }
+
+        winrt::Windows::UI::Xaml::Markup::IXamlType GetXamlType(hstring const& fullName)
+        {
+            for (const auto& provider : m_providers)
+            {
+                if (const auto xamlType = provider.GetXamlType(fullName))
+                {
+                    return xamlType;
+                }
+            }
+            return nullptr;
+        }
+
+        com_array<winrt::Windows::UI::Xaml::Markup::XmlnsDefinition> GetXmlnsDefinitions()
+        {
+            std::list<winrt::Windows::UI::Xaml::Markup::XmlnsDefinition> definitions;
+            for (const auto& provider : m_providers)
+            {
+                auto defs = provider.GetXmlnsDefinitions();
+                for (const auto& def : defs)
+                {
+                    definitions.insert(definitions.begin(), def);
+                }
+            }
+
+            return winrt::com_array<winrt::Windows::UI::Xaml::Markup::XmlnsDefinition>(definitions.begin(), definitions.end());
+        }
+
+    private:
+        winrt::Windows::UI::Xaml::Hosting::WindowsXamlManager m_windowsXamlManager{ nullptr };
+        winrt::Windows::Foundation::Collections::IVector<winrt::Windows::UI::Xaml::Markup::IXamlMetadataProvider> m_providers
+            = winrt::single_threaded_vector<Windows::UI::Xaml::Markup::IXamlMetadataProvider>();
+        bool b_closed{};
+    };
 }
 
-namespace winrt::Microsoft::Toolkit::Win32::UI::XamlHost::implementation {
-  enum ExecutionMode {
-    UWP = 0,
-    Win32 = 1,
-  };
-  using namespace winrt;
-
-  class XamlApplication : public XamlApplicationT<XamlApplication, winrt::Windows::UI::Xaml::Markup::IXamlMetadataProvider> {
-  public:
-    XamlApplication();
-    XamlApplication(winrt::Windows::Foundation::Collections::IVector<winrt::Windows::UI::Xaml::Markup::IXamlMetadataProvider> providers);
-    ~XamlApplication();
-
-    void Initialize();
-
-    void Close();
-
-    //void Run(winrt::Microsoft::Toolkit::Win32::UI::XamlHost::DesktopWindow window);
-
-    void Run();
-
-    winrt::Windows::Foundation::IClosable WindowsXamlManager() const;
-
-    winrt::Windows::UI::Xaml::Markup::IXamlType GetXamlType(winrt::Windows::UI::Xaml::Interop::TypeName const& type);
-    winrt::Windows::UI::Xaml::Markup::IXamlType GetXamlType(winrt::hstring const& fullName);
-    winrt::com_array<winrt::Windows::UI::Xaml::Markup::XmlnsDefinition> GetXmlnsDefinitions();
-
-    winrt::Windows::Foundation::Collections::IVector<winrt::Windows::UI::Xaml::Markup::IXamlMetadataProvider> MetadataProviders();
-
-    bool IsDisposed() const {
-      return m_bIsClosed;
-    }
-
-  private:
-    ExecutionMode m_executionMode = ExecutionMode::Win32;
-    winrt::Windows::UI::Xaml::Hosting::WindowsXamlManager m_windowsXamlManager = nullptr;
-    winrt::Windows::Foundation::Collections::IVector<winrt::Windows::UI::Xaml::Markup::IXamlMetadataProvider> m_providers = winrt::single_threaded_vector<Windows::UI::Xaml::Markup::IXamlMetadataProvider>();
-    bool m_bIsClosed = false;
-  };
-}
-
-namespace winrt::Microsoft::Toolkit::Win32::UI::XamlHost::factory_implementation {
-  class XamlApplication : public XamlApplicationT<XamlApplication, implementation::XamlApplication> {
-  public:
-    XamlApplication();
-    ~XamlApplication();
-  private:
-    std::array<HMODULE, 2> m_preloadInstances;
-  };
-}
-
-
-
-WINRT_EXPORT namespace winrt::Microsoft::Toolkit::Win32::UI::XamlHost {
-
-// We can't use the cppwinrt generated code for activation because it relies on the activation going through
-// LoadLibrary (inferring what library based on namespace). In the case of XamlApplication, it's a local type, so we short circuit it.
-
-  inline XamlApplication::XamlApplication(param::vector<winrt::Windows::UI::Xaml::Markup::IXamlMetadataProvider> const& providers) {
-    winrt::Windows::Foundation::IInspectable baseInterface, innerInterface;
-    auto factory = winrt::make<winrt::Microsoft::Toolkit::Win32::UI::XamlHost::factory_implementation::XamlApplication>();
-    auto self = winrt::get_self<winrt::Microsoft::Toolkit::Win32::UI::XamlHost::factory_implementation::XamlApplication>(factory);
-    *this = self->CreateInstance(providers, baseInterface, innerInterface);
-
-    // *this = impl::call_factory<XamlApplication, IXamlApplicationFactory>([&](IXamlApplicationFactory const& f) { return f.CreateInstance(providers, baseInterface, innerInterface); });
-  }
-  inline XamlApplication::XamlApplication() {
-    winrt::Windows::Foundation::IInspectable baseInterface, innerInterface;
-    auto factory = winrt::make<winrt::Microsoft::Toolkit::Win32::UI::XamlHost::factory_implementation::XamlApplication>();
-    auto self = winrt::get_self<winrt::Microsoft::Toolkit::Win32::UI::XamlHost::factory_implementation::XamlApplication>(factory);
-    *this = self->CreateInstance2(baseInterface, innerInterface);
-    //    *this = impl::call_factory<XamlApplication, IXamlApplicationFactory>([&](IXamlApplicationFactory const& f) { return f.CreateInstance2(baseInterface, innerInterface); });
-  }
-
+namespace winrt::CppXaml::factory_implementation
+{
+    struct XamlApplication : XamlApplicationT<XamlApplication, implementation::XamlApplication>
+    {
+    };
 }
--- win32/WindowsProject1.cpp
+++ win32/WindowsProject1.cpp
@@ -13,7 +13,7 @@
 #include <winrt/Windows.UI.Xaml.Interop.h>
 #include <winrt/Windows.UI.Xaml.Markup.h>
 
-#include "XamlApplication.cpp"
+#include <XamlApplication.h>
 #include <winrt/AppMarkup.h>
 
 using namespace winrt;
@@ -21,7 +21,7 @@ using namespace Windows::UI::Xaml::Controls;
 using namespace Windows::UI::Xaml;
 using namespace Windows::UI::Xaml::Hosting;
 
-using namespace Microsoft::Toolkit::Win32::UI::XamlHost;
+using namespace CppXaml;
 
 XamlApplication xapp{ nullptr };
 
@@ -55,12 +55,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     auto winuiIXMP = winrt::Microsoft::UI::Xaml::XamlTypeInfo::XamlControlsXamlMetaDataProvider();
     auto markupIXMP = winrt::AppMarkup::XamlMetaDataProvider();
-
-    xapp = XamlApplication({winuiIXMP, markupIXMP});
+    
+    xapp = winrt::make<implementation::XamlApplication>();
     // TODO: fix passing providers in ctor
     xapp.MetadataProviders().Append(winuiIXMP);
     xapp.MetadataProviders().Append(markupIXMP);
-
+    
     WindowsXamlManager winxamlmanager = WindowsXamlManager::InitializeForCurrentThread();
     xapp.Resources().MergedDictionaries().Append(winrt::Microsoft::UI::Xaml::Controls::XamlControlsResources());
 
